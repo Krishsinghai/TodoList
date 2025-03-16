42ca81a6eb1d364a71addc25bded1d8e99172fcb
Krish Singhai <153170580+Krishsinghai@users.noreply.github.com>
2025-03-16 21:32:27 +0530

    Update next.config.ts

--- Todolist/next.config.ts
+++ Todolist/next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  output: "standalone", // Ensures the app works on platforms like Render
 };
 
 export default nextConfig;
